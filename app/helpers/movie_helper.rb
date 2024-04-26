6c3424e0ead08b8c03a90518946ea37ce84523b8
jpantonov <jpantonow@gmail.com>
2024-04-26 16:04:27 -0300

    at least index.html is working

---
+++ app/helpers/movie_helper.rb
@@ -0,0 +1,5 @@
+module MovieHelper
+  def my_link_to(text, href)
+    "<a href='#{href}'>#{text}</a>".html_safe
+  end
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  def self.ratings
+    return ['G','PG','PG-13','R']
+  end
 end
